ee37c4c2e387d5900302585f36a868d50a9ab19b
j-rehn <jacqueline.rehn@adelaide.edu.au>
2021-02-09 10:00:09 +1030

    Edited helper_script.sh to bring in-line with changes made to run_km.sh. User can specify threads for jellyfish or script will default to 12 threads. User needs to replace location of targets to run against if they are in alternate location to what is provided.

--- helper_script.sh
+++ helper_script.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-### Run km on multiple samples against all target sets ###
+### Run km on multiple samples against all target sets except IGH ###
   ### Also runs Rscript to collate & filter output ###
 
 # help page
-if [ "$#" != "1" ]; then
-	echo "Usage: `basename $0` [SAMPLE_SHEET]"
+if (( $# < 1 )); then
+	echo "Usage: `basename $0` [SAMPLE_SHEET] [THREADS]"
   	echo "Incorrect number of arguments"
 	echo "- Required: SAMPLE_SHEET = Tab-delimited Text file containing FASTQ R1 and R2 locations"
 	echo ""
 	echo "<READ1 PATH> <--TAB--> <READ2 PATH>"
 	echo ""
+	echo "- Optional: THREADS = Number of threads for running jellyfish. Default is 12"
 	exit 0
 fi
 
 # Set base directory
 BASE=`pwd`
+THREAD=${2:-12}
 
 ### For loop; extracts sample data from txt file
 while read line; do
@@ -24,19 +26,16 @@ while read line; do
   R2=$(echo $line | awk '{ print $2 }')
   
   # Run_km
-  bash run_km.sh ${R1} ${R2} Fusion
-  bash run_km.sh ${R1} ${R2} DUX4
-  bash run_km.sh ${R1} ${R2} SNV
-  bash run_km.sh ${R1} ${R2} FocDel
-  bash run_km.sh ${R1} ${R2} IGH
+  bash run_km2.sh ${R1} ${R2} ${THREAD} ~/km_ALL_targets/ALL_targets/Fusion
+  bash run_km2.sh ${R1} ${R2} ${THREAD} ~/km_ALL_targets/ALL_targets/DUX4
+  bash run_km2.sh ${R1} ${R2} ${THREAD} ~/km_ALL_targets/ALL_targets/SNV
+  bash run_km2.sh ${R1} ${R2} ${THREAD} ~/km_ALL_targets/ALL_targets/focal_deletions
+  # bash run_km2.sh ${R1} ${R2} ${THREAD} ~/km_ALL_targets/ALL_targets/IGH_fusion
   
   # Directory setup for running Rscript
-  #OUTPUTS=${BASE}/output
-  SAMPLE=`basename ${R1} _1.fastq.gz`
+  SAMPLE=`basename ${R1} | sed 's/_[^_]*$//'`
   
   # Run filter_km_output.R
   Rscript bin/filter_km_output.R output/${SAMPLE}
   
 done < $1
-  
-  
